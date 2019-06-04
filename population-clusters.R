08573b206ad5b55272bddb2cb2e8edaae1acdaf5
Hrzic <R.Hrzic@unimaas.nl>
2019-06-04 15:33:51 +0200

    Added OPTICS to clustering

--- population-clusters.R
+++ population-clusters.R
@@ -22,6 +22,8 @@ abline(h = 0.9)
 
 # HDBSCAN
 
+set.seed(1)
+
 db <- dbscan(df_train, eps = 0.9, minPts = 4)
 db
 
@@ -44,10 +46,17 @@ km
 
 fviz_cluster(km, df)
 
+# OPTICS
+
+opt <- optics(df_train, eps = 1, minPts = 4)
+opt <- extractDBSCAN(opt, eps_cl = 1)
+
 # Evaluate relationship between cluster and target
 
 df$dbscan_cluster <- db$cluster
 df$km_cluster <- km$cluster
+df$opt_cluster <- opt$cluster
 
 plot(target ~ dbscan_cluster, data=df)
 plot(target ~ km_cluster, data=df)
+plot(target ~ opt_cluster, data=df)
